d3ce16fc2e95c4c6dd731a90921aef849fb9be4c
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-05-13 12:27:49 +0200

    Add Logo & Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,30 @@
 <template>
-    <nav>
-        <a href="#">{{link}}</a>
-    </nav>
+  <li class="nav-item">
+    <a class="nav-link active" href="#">{{ link }}</a>
+  </li>
 </template>
 
 <script>
 export default {
-  name: 'NavBar',
-    props:{
-        link: Array
-    }
-}
+  name: "NavBar",
+  props: {
+    link: Array,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
- 
+nav {
+  
+  font-size: 10px;
+  li {
+      padding: 0.5rem;
+      
+  }
+
+  .nav-item>a{
+      color: white;
+  }
+}
 </style>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,25 +1,36 @@
 <template>
-    <header>
-        <div class="container-fluid">
-            <div class="logo">
-                LOGO
-            </div>
-            <Navbar :link="link" v-for="(link, index) in links" :key="index"/>
-        </div>
-    </header>
+  <header>
+    <div class="container navigation">
+      <div class="logo"><img src="@/assets/img/logo-restaurant.png" alt="logo"></div>
+      <nav class="navbar">
+        <ul class="navbar-nav flex-row">
+          <Navbar :link="link" v-for="(link, index) in links" :key="index" />
+        </ul>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar.vue'
+import Navbar from "@/components/Navbar.vue";
 export default {
   name: "SiteHeader",
   components: {
-      Navbar
+    Navbar,
   },
 
   data() {
     return {
-      links: ['Home', 'Culinary History', 'Our Team', 'Our Menu', 'Takeout', 'Bulletin', 'Reservation', 'Carrello'],
+      links: [
+        "Home",
+        "Culinary History",
+        "Our Team",
+        "Our Menu",
+        "Takeout",
+        "Bulletin",
+        "Reservation",
+        "Carrello",
+      ],
     };
   },
 };
@@ -27,4 +38,17 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+header{
+    background-color: rgba(21, 21, 21, 0.66);
+}
+
+.navigation {
+  display: flex;
+  flex-wrap: wrap;
+  align-content: center;
+  align-items: center;
+  justify-content: space-between;
+}
+
+
 </style>
